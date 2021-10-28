ccc830f87d858520add44cc00623bc122612e466
asold2 <74716450+asold2@users.noreply.github.com>
2021-10-28 14:19:53 +0200

    commit with added Second Tier in Java

---
+++ small-paws-project/small-paws-project/src/test/java/smallpawsproject/smallpawsproject/SmallPawsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package smallpawsproject.smallpawsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallPawsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
